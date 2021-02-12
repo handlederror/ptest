2bf250bce63d5fd458d8ea7148eef1d854698907
handlederror <madev.na.ft@gmail.com>
2021-02-12 23:02:21 +0300

    Added verbose text to job runner

--- ptest.hpp
+++ ptest.hpp
@@ -18,13 +18,40 @@ namespace testing
         }
 
         int runTests() {
+            std::cout << "========== PTest Tester Job ==========" << std::endl << std::endl;
+
+            int testStatus;
             char* args[1] = {"--no-argument"};
 
-            return this->testJob(args);
+            std::cout << " - Running test job..." << std::endl;
+
+            testStatus = this->testJob(args);
+
+            if (testStatus != 0) {
+                std::cout << std::endl << " -- Test job done (exited with errors) -- " << std::endl << std::endl;
+            } else {
+                std::cout << std::endl << " -- Test job done (no errors) -- " << std::endl << std::endl;
+            }
+
+            return testStatus;
         }
 
         int runTests(char* args[]) {
-            return this->testJob(args);
+            std::cout << "========== PTest Tester Job ==========" << std::endl << std::endl;
+
+            int testStatus;
+
+            std::cout << " - Running test job..." << std::endl << std::endl;
+
+            testStatus = this->testJob(args);
+
+            if (testStatus != 0) {
+                std::cout << std::endl << " -- Test job done (exited with errors) -- " << std::endl << std::endl;
+            } else {
+                std::cout << std::endl << " -- Test job done (no errors) -- " << std::endl << std::endl;
+            }
+
+            return testStatus;
         }
     };
 }
